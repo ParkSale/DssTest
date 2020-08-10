6e3dd28c1bf26b8a45e4c77d8534883e7d5131e1
parksale <psi0613@naver.com>
2020-08-10 09:08:29 +0900

    update pom.xml

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,4 @@
 import io.github.ztkmkoo.dss.server.rest.DssRestServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Application {
     public static void main(String[] args) throws InterruptedException {
